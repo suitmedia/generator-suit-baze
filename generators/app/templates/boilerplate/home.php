bfff519b978a6fecc2e43411395b429add9cd289
chandra rolexta <chunderscore@gmail.com>
2020-01-13 16:05:33 +0700

    Drop modernizr

--- generators/app/templates/boilerplate/home.php
+++ generators/app/templates/boilerplate/home.php
@@ -60,7 +60,6 @@
     </section>
 
     <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=default,promise,fetch" defer></script>
-    <script src="assets/js/vendor/modernizr.min.js" defer></script>
     <script src="assets/js/vendor/jquery.min.js" defer></script>
     <script src="assets/js/vendor/object-fit-images.min.js" defer></script>
     <script src="assets/js/vendor/sprintf.min.js" defer></script>
